a6bd5b6e78fbdd6692fff9195c3483c024f932dd
Andy Gill <andygill@ku.edu>
2014-08-05 22:31:17 -0500

    Move Prose datatype around.

--- src/Graphics/Storyboard/Prose.hs
+++ src/Graphics/Storyboard/Prose.hs
@@ -20,6 +20,30 @@ import Graphics.Storyboard.Literals
 
 ------------------------------------------------------------------------
 
+newtype Prose = Prose [Either Float Word]
+  deriving Show
+
+
+instance IsString Prose where
+  fromString txt = Prose $ List.intersperse (Left 1)
+      [ Right $ Word [] $ Text.pack $ wd -- default is *no* annotations
+      | wd <- words txt
+      ]
+
+instance Semigroup Prose where
+  (Prose xs) <> (Prose ys) = Prose (xs++ys)
+
+instance Monoid Prose where
+  mempty = Prose []
+  mappend (Prose xs) (Prose ys) = Prose (xs++ys)
+
+super :: Prose -> Prose
+super (Prose ps) = Prose $ map f ps
+  where f (Left n) = Left (n / 0.7)
+        f (Right (Word es txt)) = Right (Word (Super:es) txt)
+
+------------------------------------------------------------------------
+
 -- [font style] [font weight] [font size] [font face]
 -- style = normal | italic | oblique | inherit
 -- weight = normal | bold | bolder | lighter | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | inherit | auto
@@ -45,36 +69,10 @@ instance Show Emphasis where
 
 ------------------------------------------------------------------------
 
-data Word
-      = Word [Emphasis] Text
---      | WordSpace Float      -- 1 for space, 0 for (breakable) 0-width-space
+data Word = Word [Emphasis] Text
 
 instance Show Word where
    show (Word [] txt)   = show $ Text.unpack txt
    show (Word emph txt) = show emph ++ show (Text.unpack txt)
---   show (WordSpace n)   = show n
-
 
 ------------------------------------------------------------------------
-
-newtype Prose = Prose [Either Float Word]
-  deriving Show
-
-
-instance IsString Prose where
-  fromString txt = Prose $ List.intersperse (Left 1)
-      [ Right $ Word [] $ Text.pack $ wd -- default is *no* annotations
-      | wd <- words txt
-      ]
-
-instance Semigroup Prose where
-  (Prose xs) <> (Prose ys) = Prose (xs++ys)
-
-instance Monoid Prose where
-  mempty = Prose []
-  mappend (Prose xs) (Prose ys) = Prose (xs++ys)
-
-super :: Prose -> Prose
-super (Prose ps) = Prose $ map f ps
-  where f (Left n) = Left (n / 0.7)
-        f (Right (Word es txt)) = Right (Word (Super:es) txt)
