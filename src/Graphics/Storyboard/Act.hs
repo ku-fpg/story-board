a812b13788411e5d60e68188c28ad6a36a6739d8
Andy Gill <andygill@ku.edu>
2014-08-15 15:13:56 -0500

    Draft of Act monad.

--- src/Graphics/Storyboard/Act.hs
+++ src/Graphics/Storyboard/Act.hs
@@ -1,34 +1,39 @@
+{-# LANGUAGE KindSignatures, TupleSections, GADTs, GeneralizedNewtypeDeriving, InstanceSigs, OverloadedStrings #-}
+
 module Graphics.Storyboard.Act where
 
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
 import Graphics.Blank
 
-data Act a = Act
-  { thisAct :: Canvas a
-  , nextAct :: Act ()
-  }
-            | Done
+import Graphics.Storyboard.Types
 
+newtype Act a = Act { runAct :: Canvas (a,[NextAct]) }
 
-futureAct :: (...pred...) -> Act () -> Act ()
+data NextAct where
+  NextAnimationFrame :: Act () -> NextAct
 
+nextAnimationFrame :: Act () -> Act ()
+nextAnimationFrame = Act .return . ((),) . (:[]) . NextAnimationFrame
 
+instance MonadCanvas Act where
+  liftCanvas m = Act $ fmap (,[]) m
 
-
-{-
-instance Functor Prelude where
+instance Functor Act where
  fmap f m = pure f <*> m
 
-instance Applicative Prelude where
+instance Applicative Act where
   pure = return
   f <*> a = liftM2 ($) f a
--}
-instance Monad Act where
-  return a = Act (return a) Done
---  Prelude m n >>= k = Act $ do
---    r <- m q
---    runPrelude (k r) qa
 
-{-
-instance MonadIO Prelude where
-    liftIO = Prelude . const . liftIO
--}
+instance Monad Act where
+  return = Act . return . (,[])
+  Act m >>= k = Act $ do
+      (a,nx1) <- m
+      (r,nx2) <- runAct (k a)
+      return (r,nx1 ++ nx2)
+
+
+instance MonadIO Act where
+    liftIO = Act . fmap (,[]) . liftIO
