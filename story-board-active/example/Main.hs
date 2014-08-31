7feb3fbdef062fe6f589d7bf9313ae1c1f4e605a
Andy Gill <andygill@ku.edu>
2014-08-31 17:38:46 -0500

    Adding example of active.

--- story-board-active/example/Main.hs
+++ story-board-active/example/Main.hs
@@ -2,10 +2,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Graphics.Storyboard.Active as A
+import Graphics.Storyboard.Active() -- for the instance
 
 import Graphics.Blank             as B
 import Graphics.Storyboard        as SB
+
+import Data.Active
 
 main :: IO ()
 main = SB.storyBoard [ slide ]
@@ -13,13 +15,15 @@ main = SB.storyBoard [ slide ]
 slide :: SB.Slide ()
 slide = do
       SB.align SB.center $ SB.p $ "Active Plugin Example"
-{-
-      let t = SB.drawTile (200,200) dig
 
-      SB.place SB.top (SB.nudge SB.top SB.center t)
+      let t = SB.drawMovieTile (200,200) $ mkActive 0 10 $ \ tm -> do
+                  beginPath()
+                  rect(1,1,20 * fromTime tm,20 * fromTime tm)
+                  strokeStyle "red"
+                  lineWidth 1
+                  closePath()
+                  stroke()
 
-      let t = SB.drawMovieTile (200,200) (pad 1.05 . tournament . succ. (`mod` 10) . (floor :: (Float -> Int)))
+      SB.place SB.top (SB.nudge SB.center SB.center t)
 
-      SB.place SB.top (SB.nudge SB.top SB.center t)
--}
       return ()
