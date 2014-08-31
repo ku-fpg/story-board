182c7d97fb4b39a09a40dd0ab76ec84f6db3fe42
Andy Gill <andygill@ku.edu>
2014-08-31 00:12:20 -0500

    Adding Diagrams plugin

---
+++ story-board-diagrams/Graphics/Storyboard/Diagrams.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE NoMonomorphismRestriction, TypeSynonymInstances, FlexibleInstances #-}
+module Graphics.Storyboard.Diagrams where
+
+-- This is the diagrams plugin
+
+import Graphics.Storyboard.Types
+
+import Diagrams.Backend.Canvas
+import Diagrams.Prelude
+
+instance Drawing (Diagram Canvas R2) where
+   drawCanvas d (w,h) = renderDia Canvas opts d
+    where opts = CanvasOptions $ Dims (fromIntegral $ round w)
+    	       	 	       	      (fromIntegral $ round h)
