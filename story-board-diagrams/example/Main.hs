b1a3312500f54d6be267162e2e94d0e3d4a5440b
Andy Gill <andygill@ku.edu>
2014-08-31 11:06:30 -0500

    Example of using Diagrams in story board

--- story-board-diagrams/example/Main.hs
+++ story-board-diagrams/example/Main.hs
@@ -1,3 +1,25 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main = print "Hello"
+import Diagrams.Prelude as Dia
+import Diagrams.Backend.Canvas
+import Graphics.Storyboard.Diagrams
+
+import Graphics.Storyboard as SB
+
+main :: IO ()
+main = storyBoard [ slide ]
+
+slide :: Slide ()
+slide = do
+      SB.align SB.center $ p $ "Diagrams Plugin Example"
+      
+      let t = drawTile dig (200,200)
+
+      SB.place top t
+
+      return ()
+
+dig :: Diagram B R2
+dig = circle 1
